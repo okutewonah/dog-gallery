e183b19deafe518aed098da36e685537da03c176
Okute Wonah <okutewonah@gmail.com>
2021-02-24 22:13:58 +0200

    implement bootstrap components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import BreedGrid from './components/BreedGrid';
+import Wave from 'react-wavify'
 
 function App() {
   const [dogs, setDogs] = useState([]);
@@ -19,8 +20,16 @@ function App() {
   },[dogs])
 
   return (
-    <div className="App">
-      <h1 className='text-uppercase mt-lg-5'>Dog Gallery</h1>
+    <div className="App mx-2">
+      <Wave fill="url(#gradient)">
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="20%"  stopColor="#ffe666" />
+            <stop offset="80%" stopColor="#db3522" />
+          </linearGradient>
+        </defs>
+      </Wave>
+      <h1 className='text-uppercase my-lg-5 font-weight-bold'>Dog Gallery</h1>
       <BreedGrid dogs={dogs} />
     </div>
   );
--- src/components/BreedsList.js
+++ src/components/BreedsList.js
@@ -1,14 +1,16 @@
-import { Col, Image } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
 
 const BreedsList = ({ breed }) => {
     return (
         <Col md={3} sm={6}>
-            <div className='gallery-item'>
-                <Image src={breed.image.url} width={400} height={400} alt={breed.name}  />
-                <h3>Name: {breed.name}</h3>
-                <p>Bred for: {breed.bred_for}</p>
-                <p>Temperaments: {breed.temperament}</p>
-            </div>
+            <Card className='gallery-item'>
+                <Card.Body>
+                    <Card.Img variant='top' fluid src={breed.image.url} width={400} height={400} alt={breed.name} style={{ width: '100%', height: 'auto' }}  />
+                    <Card.Title className='mt-2'>Name: {breed.name}</Card.Title>
+                    <Card.Text>Bred for: {breed.bred_for}</Card.Text>
+                    <Card.Text>Temperaments: {breed.temperament}</Card.Text>
+                </Card.Body>
+            </Card>
         </Col>
     )
 }
