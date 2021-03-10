ba4d183c0cccd1ce982c17e991f454b1b2d519fb
Okute Wonah <okutewonah@gmail.com>
2021-03-10 16:45:44 +0200

    added pagination

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import BreedGrid from './components/BreedGrid';
+import Pagination from './components/Pagination';
 import Wave from 'react-wavify'
 
 function App() {
   const [dogs, setDogs] = useState([]);
   // const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1)
+  const [dogsPerPage] = useState(12)
 
   useEffect(() => {
     const fetchDogs = async () => {
@@ -19,6 +22,13 @@ function App() {
     fetchDogs();
   },[dogs])
 
+  // Get current posts
+  const indexOfLastPost = currentPage * dogsPerPage
+  const indexOfFirstPost = indexOfLastPost - dogsPerPage
+  const currentPages = dogs.slice(indexOfFirstPost, indexOfLastPost)
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="App mx-2">
       <Wave fill="url(#gradient)">
@@ -30,7 +40,8 @@ function App() {
         </defs>
       </Wave>
       <h1 className='text-uppercase my-lg-5 font-weight-bold'>Dog Gallery</h1>
-      <BreedGrid dogs={dogs} />
+      <BreedGrid dogs={currentPages} />
+      <Pagination dogsPerPage={dogsPerPage} totalDogs={dogs.length} paginate={paginate} />
     </div>
   );
 }
